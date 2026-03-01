38276e90fcbceaab354392f54140c51c9a189d25
nancykhalid88 <42310142.Nancy@acu.edu.eg>
2026-03-01 19:19:52 +0200

    Q1

--- assignment3/Program.cs
+++ assignment3/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Part01
+            #region Q1
+            //Q1 : Identify the type of relationship in each scenario below (Inheritance, Association, Aggregation, Composition, or Dependency):
+
+            //a) A University has Departments. If the university is closed, the departments no longer exist.
+            //Composition
+
+            //b) A Driver uses a Car.The driver does not own the car.
+            //Association
+
+            //c) A Dog is an Animal.
+            //Inheritance
+
+            //d) A Team has Players. If the team is deleted, the players still exist.
+            //Aggregation
+
+            //e) A method receives a Logger as a parameter and calls it inside the method only.
+            //Dependency 
+            #endregion
+
+
+           
         }
     }
 }
